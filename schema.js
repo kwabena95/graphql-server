d68f9cd14875782a5f370d33515c3513df4c8ffa
ebenezer <e.kankam95@hotmail.com>
2021-05-05 16:29:56 -0500

    installed axios for http request and created a data.json file

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,27 +8,6 @@ const {
     GraphQLNonNull,
 } = require('graphql');
 
-// Hardcoded data
-const customers = [
-    {
-        id: "1",
-        name: "John Doe",
-        email: "jdoe@mail.com",
-        age: 35
-    },
-    {
-        id: "2",
-        name: "Jane Doe",
-        email: "janeD@mail.com",
-        age: 25
-    },
-    {
-        id: "3",
-        name: "Steve Smith",
-        email: "boywonder@mail.com",
-        age: 16
-    },
-]
 
 // Customer type
 const CustomerType = new GraphQLObjectType({
@@ -52,18 +32,22 @@ const RootQuery = new GraphQLObjectType({
                 }
             },
             resolve(parentValue, args) {
-                for (let i = 0; i < customers.length; i++) {
-                    if (customers[i].id == args.id) {
-                        return customers[i];
-                    }
+                // for (let i = 0; i < customers.length; i++) {
+                //     if (customers[i].id == args.id) {
+                //         return customers[i];
+                //     }
 
-                }
+                // }
+
+                return axios.get(`http://localhost:3000/customers/${args.id}`)
+                    .then(res => res.data)
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                return axios.get(`http://localhost:3000/customers`)
+                    .then(res => res.data)
             }
         }
     }
